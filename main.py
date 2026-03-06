4a681a1a496c289bf93487c21200c0b9cb7fb2d0
Rathore74 <2501001094@cgcjhanjeri.in>
2026-03-06 22:06:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes import router
 
 app = FastAPI()
 
-# This connects the routes created by your "team members"
+# Mount the static folder for CSS
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Include the routes developed by the team
 app.include_router(router)
 
 if _name_ == "_main_":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
